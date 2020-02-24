20cf2b2c236e80a38c835f659450288974d422ce
Alex Zarr <alexander.zarr@aequilibrium.ca>
2020-02-24 15:12:00 -0800

    Add DBHelperProtocol

---
+++ DemoToDoList/Modules/DBHelper/DBHelper/DBHelper.h
@@ -0,0 +1,19 @@
+//
+//  DBHelper.h
+//  DBHelper
+//
+//  Created by Alex Zarr on 2020-02-24.
+//  Copyright © 2020 alexzarr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DBHelper.
+FOUNDATION_EXPORT double DBHelperVersionNumber;
+
+//! Project version string for DBHelper.
+FOUNDATION_EXPORT const unsigned char DBHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DBHelper/PublicHeader.h>
+
+
---
+++ DemoToDoList/Modules/DBHelper/DBHelper/Sources/DBHelper.swift
@@ -0,0 +1,26 @@
+//
+//  DBHelper.swift
+//  DBHelper
+//
+//  Created by Alex Zarr on 2020-02-24.
+//  Copyright © 2020 alexzarr. All rights reserved.
+//
+
+import Foundation
+
+public protocol DBHelperProtocol {
+    associatedtype ObjectType
+    associatedtype PredicateType
+    
+    func create(_ object: ObjectType)
+    func fetchFirst(_ objectType: ObjectType.Type, predicate: PredicateType?) -> Result<ObjectType?, Error>
+    func fetch(_ objectType: ObjectType.Type, predicate: PredicateType?, limit: Int?) -> Result<[ObjectType], Error>
+    func update(_ object: ObjectType)
+    func delete(_ object: ObjectType)
+}
+
+public extension DBHelperProtocol {
+    func fetch(_ objectType: ObjectType.Type, predicate: PredicateType? = nil, limit: Int? = nil) -> Result<[ObjectType], Error> {
+        return fetch(objectType, predicate: predicate, limit: limit)
+    }
+}
